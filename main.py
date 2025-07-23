d238bf33a776a497e8f53f016b2dba8cf8917872
geekrun <11956884+geekrun@users.noreply.github.com>
2025-07-23 18:33:04 +0800

    feat: hello接口返回当前时间，增加随机性演示

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import os
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    now = datetime.datetime.now().isoformat()
+    return f"Hello, World! Now: {now}"
 
 
 if __name__ == "__main__":
